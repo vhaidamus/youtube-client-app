ee6424c7cd57b615368a09643841cf86bd1135b4
vhaidamus <vhaidamus@gmail.com>
2020-06-09 14:49:29 +0100

    create xcode project

---
+++ youtube-client-app/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  youtube-client-app
+//
+//  Created by Victoria Haidamus on 09/06/20.
+//  Copyright © 2020 Victoria Haidamus. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
